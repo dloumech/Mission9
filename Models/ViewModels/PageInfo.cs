86debf4d68242df798cf4d242b03bf47e101cdae
Darby Mecham <dloumech@byu.edu>
2023-03-02 00:53:26 -0700

    Add project files.

---
+++ Models/ViewModels/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        //calc pages needed 
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumBooks / BPerPage);
+
+    }
+}
